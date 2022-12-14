29bbd0fbef19a36da90dd3e15244099731da48dc
Matheus Martins <matheus87martins@outlook.com>
2022-12-14 07:53:21 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Department obj = new Department(1, "Book");
+
+        System.out.println(obj);
     }
 }
