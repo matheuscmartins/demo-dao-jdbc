b9df4100fcebe8845b40f8cbe5050ab51ebbf2cc
Matheus Martins <matheus87martins@outlook.com>
2022-12-14 08:43:03 -0300

    findById implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import Db.DB;
+import Db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection connection;
+    public SellerDaoJDBC(Connection conn){
+        this.connection= conn;
+    }
+
     @Override
     public void insert(Seller seller) {
 
@@ -23,7 +36,40 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                    +"FROM seller INNER JOIN department "
+                    +"ON seller.DepartmentId = department.Id "
+                    +"WHERE seller.Id = ?"
+            );
+            statement.setInt(1, id);
+            resultSet = statement.executeQuery();
+            if (resultSet.next()){
+
+                Department objDepartment = new Department();
+                objDepartment.setId(resultSet.getInt("DepartmentId"));
+                objDepartment.setName(resultSet.getString("DepName"));
+                Seller objSeller = new Seller();
+                objSeller.setId(resultSet.getInt("Id"));
+                objSeller.setName(resultSet.getString("Name"));
+                objSeller.setEmail(resultSet.getString("Email"));
+                objSeller.getBaseSalary(resultSet.getDouble("BaseSalary"));
+                objSeller.setBirthDate(resultSet.getDate("BirthDate"));
+                objSeller.setDepartment(objDepartment);
+                return objSeller;
+            }
+            return null;
+        } catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(statement);
+            DB.closeResultSet(resultSet);
+        }
+
     }
 
     @Override
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -59,8 +59,8 @@ public class Seller implements Serializable {
         this.birthDate = birthDate;
     }
 
-    public Double getBaseSalary() {
-        return baseSalary;
+    public Double getBaseSalary(double baseSalary) {
+        return this.baseSalary;
     }
 
     public void setBaseSalary(Double baseSalary) {
